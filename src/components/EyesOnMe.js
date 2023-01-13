a507575c03d9b8544c5bf9b3a0185dbf76c7424a
barsu5489 <ebarsulai@gmail.com>
2023-01-13 08:49:11 +0300

    event handler handled

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,9 +1,17 @@
 // Code EyesOnMe Component Here
-import React from 'react'
-
+import React, {useState} from 'react'
+//const [blur, setBlur] = useState(true)
 function EyesOnMe() {
+
+    function handleFocus(){
+        
+        console.log('Good!')
+    }
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
   return (
-    <div><button>Eyes on me</button></div>
+    <div><button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button></div>
   )
 }
 
